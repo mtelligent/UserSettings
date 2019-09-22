798b0254c9ff5769b5f6029522e14a363010f3ca
David San Filippo <david@mtelligent.com>
2019-09-22 19:20:06 -0400

    Add project files.

---
+++ src/Foundation/Facets/code/sitecore/shell/client/Applications/ExperienceProfile/Contact/UserSettings.js
@@ -0,0 +1,57 @@
+﻿define(["sitecore", "/-/speak/v1/experienceprofile/CintelUtl.js"], function (sc, cintelUtil) {
+
+    var selectedTabProperty = "selectedTab";
+    var textProperty = "text";
+
+    var app = sc.Definitions.App.extend({
+        initialized: function () {
+            this.processTabs();
+        },
+
+        loadPanel: function () {
+            var panelId = $("[data-sc-id='UserSettingsTabControl'] > .tab-content > .active .sc-load-on-demand-panel").data("sc-id");
+            var panel = this[panelId];
+
+            if (panel && !panel.get("isLoaded")) {
+                panel.on("change:isLoaded", function () {
+                    panel.set("isBusy", false);
+                });
+
+                panel.set("isBusy", true);
+                panel.load();
+            }
+        },
+
+        showDefaultTab: function () {
+            var firstTabId = this.UserSettingsTabControl.get(selectedTabProperty);
+            var urlTabId = this.getTabIdFromUrl();
+
+            if (urlTabId && urlTabId != firstTabId) {
+                this.UserSettingsTabControl.set(selectedTabProperty, urlTabId);
+            } else {
+                this.loadPanel();
+            }
+        },
+
+        processTabs: function () {
+            this.UserSettingsTabControl.on("change:" + selectedTabProperty, function (tabControl, selectedTab) {
+                this.loadPanel();
+            }, this);
+
+            this.showDefaultTab();
+        },
+
+        getTabIdFromUrl: function () {
+            var tabName = cintelUtil.getQueryParam("subtab");
+            if (!tabName) return null;
+
+            var tabIdControlId = tabName[0].toUpperCase() + tabName.toLowerCase().substring(1) + "TabId";
+            var tabIdControl = this[tabIdControlId];
+            if (!tabIdControl) return null;
+
+            return tabIdControl.get(textProperty);
+        }
+
+    });
+    return app;
+});
---
+++ src/Foundation/Facets/code/sitecore/shell/client/Applications/FormsBuilder/Layouts/Actions/SaveToUserSettings.js
@@ -0,0 +1,41 @@
+﻿(function (speak) {
+    var parentApp = window.parent.Sitecore.Speak.app.findApplication('EditActionSubAppRenderer');
+
+    speak.pageCode(["underscore"],
+        function (_) {
+            return {
+                initialized: function () {
+                    this.on({
+                        "loaded": this.loadDone
+                    },
+                        this);
+
+                    if (parentApp) {
+                        parentApp.loadDone(this, this.HeaderTitle.Text, this.HeaderSubtitle.Text);
+                        parentApp.setSelectability(this, true);
+                    }
+                },
+
+                loadDone: function (parameters) {
+                    this.Parameters = parameters || {};
+                },
+
+                getData: function () {
+                    var formData = this.SetAreaForm.getFormData(),
+                        keys = _.keys(formData);
+
+                    keys.forEach(function (propKey) {
+                        if (formData[propKey] == null || formData[propKey].length === 0) {
+                            if (this.Parameters.hasOwnProperty(propKey)) {
+                                delete this.Parameters[propKey];
+                            }
+                        } else {
+                            this.Parameters[propKey] = formData[propKey];
+                        }
+                    }.bind(this));
+
+                    return this.Parameters;
+                }
+            };
+        });
+})(Sitecore.Speak);
